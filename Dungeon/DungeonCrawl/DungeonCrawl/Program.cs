b4b547c5932fab638dd91ce4e3e4ab0a2c734cd7
IakonaColobong <jason.colobong@outlook.com>
2022-02-18 15:55:39 -0600

    Added Console interface

--- Dungeon/DungeonCrawl/DungeonCrawl/Program.cs
+++ Dungeon/DungeonCrawl/DungeonCrawl/Program.cs
@@ -10,6 +10,78 @@ namespace DungeonCrawl
     {
         static void Main(string[] args)
         {
+
+            int CharHpCurrent = 20;
+            int CharHpFull = 100;
+            int CharXpCurrent = 10;
+            int CharXpFull = 100;
+            int CharLevelCurrent = 1;
+
+            int CharLeftRight = 10;
+            int CharUpDown = 10;
+            int Dead = 0;
+
+            //String
+            string CharName = "Jason";
+
+            do
+            {
+                //DRAW GUI
+
+                //STATS
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(" ______________________________________________________________________________________________________________________");
+                Console.SetCursorPosition(5, 1);
+                Console.WriteLine("Name: {0}", CharName);
+                Console.SetCursorPosition(45, 1);
+                Console.WriteLine("HP: {0}/{1}", CharHpCurrent, CharHpFull);
+                Console.SetCursorPosition(85, 1);
+                Console.WriteLine("Experience: {0}/{1}", CharXpCurrent, CharXpFull);
+                Console.WriteLine("|______________________________________________________________________________________________________________________|");
+                Console.SetCursorPosition(0, 1);
+                Console.WriteLine("|");
+                Console.SetCursorPosition(119, 1);
+                Console.WriteLine("|");
+                //GAME SCREEN
+
+                for (int a = 3; a <= 27; a++)
+                {
+                    Console.SetCursorPosition(0, a);
+                    Console.Write("|");
+                    Console.SetCursorPosition(119, a);
+                    Console.Write("|");
+                }
+                Console.WriteLine("|______________________________________________________________________________________________________________________|");
+
+                ConsoleKeyInfo KeyInfo;
+                KeyInfo = Console.ReadKey(true);
+                switch (KeyInfo.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        CharLeftRight++;
+                        Console.SetCursorPosition(CharLeftRight, CharUpDown);
+                        Console.WriteLine("X");
+                        break;
+                    case ConsoleKey.Tab:
+                        break;
+                    case ConsoleKey.Clear:
+                        break;
+                    case ConsoleKey.Enter:
+                        break;
+                    case ConsoleKey.Pause:
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (Dead == 0);
+
+   
+
+
+
+
         }
     }
 }
